b28c34ee15bafd52ca2c0c3b6332736d0415585e
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-03-07 16:27:43 -0300

    Styled navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,47 @@ const user = useUser();
 </script>
 <template>
   <div>
-    <nav style="display: flex; gap: 10px">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/movies">Movies</NuxtLink>
-      <NuxtLink v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
-      <a href="#" v-else @click.prevent="user.isLoggedIn = false">Logout</a>
+    <nav class="nav-bar">
+      <NuxtLink class="nav-bar-button" to="/">Home</NuxtLink>
+      <NuxtLink class="nav-bar-button" to="/movies">Movies</NuxtLink>
+      <NuxtLink class="nav-bar-button" v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
+      <a class="nav-bar-button" href="#" v-else @click.prevent="user.isLoggedIn = false">Logout</a>
     </nav>
     <slot></slot>
   </div>
 </template>
+<style lang="scss" scoped>
+  $background: #0c0c0d;
+  $primary-50: #F2FDF9;
+  $primary-100: #E6FCF3;
+  $primary-200: #BFF6E0;
+  $primary-300: #99F1CD;
+  $primary-400: #4DE7A8;
+  $primary-500: #00DC82;
+  $primary-600: #00C675;
+  $primary-700: #00844E;
+  $primary-800: #00633B;
+  $primary-900: #004227;
+  
+  .nav-bar{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    margin: 15px auto;
+    max-width: 400px;
+    .nav-bar-button {
+      font-weight: 700;
+      text-decoration: none;
+      border: solid 2px $primary-800;
+      padding: 10px;
+      border-radius: 50px;
+      text-align: center;
+      transition: .5s;
+      &:hover {
+        border: solid 2px $primary-600;
+        background-color: $primary-600;
+      }
+    }
+  }
+</style>
